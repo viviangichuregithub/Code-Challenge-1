00c9cb5a28f70c7c9d7244feff52c695ff2a28fb
Vivian Gichure <viviangichure@gmail.com>
2025-06-06 18:25:52 +0300

    BodaFareCalculator

---
+++ bodaFareCalculator/fare.js
@@ -0,0 +1,15 @@
+function calculateBodaFare(bodaBodaFare){
+    const baseFare = 50;
+    const farePerKilometer = 15;
+    const totalFare = baseFare + (farePerKilometer * bodaBodaFare);
+//this is what will display on the console after the user enters the distance to be traveled
+ console.log(`Umeamua kufika uko? Hiyo ni ${bodaBodaFare} Kilometer!`);
+ console.log(`Kukalia boda ni ${baseFare} bob!`);
+ console.log(`Kutoka hapa hadi huko ni ksh${farePerKilometer * bodaBodaFare} ,coz tunalipisha ${farePerKilometer} bob per every Kilometer`);
+ console.log(`Bill yote inakuja ksh${totalFare}`);
+ console.log("\nTwende! Boda iko ready!");
+
+}
+//prompts the user to enter the distance they want to travel
+const distance = Number(prompt("Unafika wapi Mkubwa? Kilometer ngapi?:"));
+calculateBodaFare(distance);
--- chaiCalculator/chai.js
+++ chaiCalculator/chai.js
@@ -5,14 +5,14 @@ function calculateChaiIngredients(numberOfChaiCups) {
     const sukari = 2 * numberOfChaiCups; 
 
 //this is what will display on the console after the user enters the number o cups needed to be made  
-console.log(`To make ${numberOfChaiCups} cups of  Kenyan Chai, you will need:`);
-    console.log(`Water: ${water} ml`);
-    console.log(`Milk: ${milk} ml`);
-    console.log(`Tea Leaves (Majani): ${majani} tablespoons`);
-    console.log(`Sugar (Sukari): ${sukari} teaspoons`);
-    console.log(`\nEnjoy your Chai Bora!`);// /n is used to add a new line before the next message
+console.log(`To make ${numberOfChaiCups} cups of Chai, unahitaji:`);
+    console.log(`${water} ml of water`);
+    console.log(`${milk} ml of milk`);
+    console.log(`${majani} tablespoons of majani chai`);
+    console.log(`${sukari} teaspoons of sugar`);
+    console.log(`\nKaribu chai`);// /n is used to add a new line before the next message
     }
 //prompts the user to enter the number of cups they want to make
-const chaiCups = Number(prompt("Karibu! How many cups of Chai Bora would you like to make?"));
+const chaiCups = Number(prompt("Karibu! How many cups of Tea would you like to make?"));
 calculateChaiIngredients(chaiCups);
 
